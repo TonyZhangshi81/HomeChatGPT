50af80ea29b29a8dcd9bcabc063fe6cd67bab75a
xzhangchenggang <tonyzhangshi@163.com>
2024-03-19 10:44:56 +0800

    聊天窗口UI调整

--- src/webApp/wwwroot/js/site.js
+++ src/webApp/wwwroot/js/site.js
@@ -1,5 +1,10 @@
 ﻿$(function () {
 
+  $('#action_menu_btn').on("click", function () {
+    $('.action_menu').toggle();
+  });
+
+
   var $chatBox = $(".card-body");
   var $messageInput = $("#message-input");
   var $sendButton = $("#send-button");
@@ -14,28 +19,34 @@
   $sendButton.on("click", function () {
     var message = $messageInput.val().trim();
 
-    const messageData = {
-      content: message,
-      time: '8:40 AM, Today',
-      avatarSrc: '/img/_M.jpg'
-    };
-
-    const $msgContainer = $('<div>', { class: 'd-flex justify-content-end mb-4' });
-    const $imgContainer = $('<div>', { class: 'msg_cotainer_send' });
-    const $avatar = $('<img>', { src: messageData.avatarSrc, class: 'rounded-circle user_img_msg' });
-    const $msgContent = $('<div>', { class: 'img_cont_msg' });
-    const $message = $('<span>').text(messageData.content);
-    const $time = $('<span>', { class: 'msg_time_send' }).text(messageData.time);
-
-    $imgContainer.append($message, $time);
-    $msgContent.append($avatar);
-    $msgContainer.append($imgContainer, $msgContent);
-
-    // 将创建的元素追加到页面上合适的位置
-    $chatBox.append($msgContainer);
-
     if (message !== "") {
       $messageInput.val("");
+
+      const messageData = {
+        content: message,
+        // TODO 时间需要更新
+        time: '8:40 AM, Today',
+        avatarSrc: '/img/_M.jpg'
+      };
+
+      const $msgContainer = $('<div>', { class: 'd-flex justify-content-end mb-4' });
+      const $imgContainer = $('<div>', { class: 'msg_cotainer_send' });
+      const $avatar = $('<img>', { src: messageData.avatarSrc, class: 'rounded-circle user_img_msg' });
+      const $msgContent = $('<div>', { class: 'img_cont_msg' });
+      const $message = $('<span>').text(messageData.content);
+      const $time = $('<span>', { class: 'msg_time_send' }).text(messageData.time);
+
+      // 提问信息显示
+      $imgContainer.append($message, $time);
+      $msgContent.append($avatar);
+      $msgContainer.append($imgContainer, $msgContent);
+
+      // 将创建的元素追加到页面上合适的位置
+      $chatBox.append($msgContainer);
+      // 滚动条移动至最后位置
+      $chatBox.scrollTop($chatBox[0].scrollHeight);
+
+
       // api调用
       $.ajax({
         type: "POST",
@@ -45,6 +56,7 @@
 
           const answerData = {
             content: data.answer,
+            // TODO 时间需要更新
             time: '8:40 AM, Today',
             avatarSrc: '/img/_D.jpg'
           };
@@ -56,12 +68,16 @@
           const $message = $('<span>').text(answerData.content);
           const $time = $('<span>', { class: 'msg_time' }).text(answerData.time);
 
+          // 应答信息显示
           $msgContent.append($avatar);
           $imgContainer.append($message, $time);
           $msgContainer.append($msgContent, $imgContainer);
 
           // 将创建的元素追加到页面上合适的位置
           $chatBox.append($msgContainer);
+          // 滚动条移动至最后位置
+          $chatBox.scrollTop($chatBox[0].scrollHeight);
+
         },
         error: function () {
           alert("出现错误,请重试");
