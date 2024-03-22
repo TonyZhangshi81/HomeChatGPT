e7132da0916bb0d8f3342c0f811d83649336ef4e
xzhangchenggang <tonyzhangshi@163.com>
2024-03-22 18:41:55 +0800

    添加check online和測試相關

--- src/webApp/wwwroot/js/site.js
+++ src/webApp/wwwroot/js/site.js
@@ -9,6 +9,9 @@
   var $messageInput = $("#message-input");
   var $sendButton = $("#send-button");
   var $icon = $('#onlineStatus');
+  var $block = $('#block');
+  var $checkOnline = $('#checkOnline');
+
 
   // 调整页面高度
   adjustDivHeight();
@@ -73,29 +76,7 @@
     if (message !== "") {
       $messageInput.val("");
 
-      const messageData = {
-        content: message,
-        time: formatAMPM(),
-        avatarSrc: '/img/_M.jpg'
-      };
-
-      const $msgContainer = $('<div>', { class: 'd-flex justify-content-end mb-4' });
-      const $imgContainer = $('<div>', { class: 'msg_cotainer_send' });
-      const $avatar = $('<img>', { src: messageData.avatarSrc, class: 'rounded-circle user_img_msg' });
-      const $msgContent = $('<div>', { class: 'img_cont_msg' });
-      const $message = $('<span>').text(messageData.content);
-      const $time = $('<span>', { class: 'msg_time_send' }).text(messageData.time);
-
-      // 提问信息显示
-      $imgContainer.append($message, $time);
-      $msgContent.append($avatar);
-      $msgContainer.append($imgContainer, $msgContent);
-
-      // 将创建的元素追加到页面上合适的位置
-      $chatBox.append($msgContainer);
-      // 滚动条移动至最后位置
-      $chatBox.scrollTop($chatBox[0].scrollHeight);
-
+      selfTalk(message);
 
       // api调用
       $.ajax({
@@ -103,34 +84,7 @@
         url: "/Home/ChatWithGPT",
         data: { input: message },
         success: function (data) {
-
-          const answerData = {
-            content: data.answer,
-            time: formatAMPM(),
-            avatarSrc: '/img/_D.jpg'
-          };
-
-          const $msgContainer = $('<div>', { class: 'd-flex justify-content-start mb-4' });
-          const $msgContent = $('<div>', { class: 'img_cont_msg' });
-          const $avatar = $('<img>', { src: answerData.avatarSrc, class: 'rounded-circle user_img_msg' });
-          const $imgContainer = $('<div>', { class: 'msg_cotainer' });
-          const $message = $('<span>').text('');
-          const $time = $('<span>', { class: 'msg_time' }).text(answerData.time);
-          const $noPrint = $('<span>', { class: 'noPrint' }).text("STOP");
-
-          // 应答信息显示
-          $msgContent.append($avatar);
-          $imgContainer.append($message, $time, $noPrint);
-          $msgContainer.append($msgContent, $imgContainer);
-
-          // 将创建的元素追加到页面上合适的位置
-          $chatBox.append($msgContainer);
-          // 滚动条移动至最后位置
-          $chatBox.scrollTop($chatBox[0].scrollHeight);
-
-          // 文字打印
-          printString(answerData.content, $message, $chatBox);
-
+          robotTalk(data.answer);
         },
         error: function () {
           alert("出现错误,请重试");
@@ -139,133 +93,203 @@
     }
   });
 
-  const isStop = true;
-  if (isStop) {
-    $icon.removeClass('offline');
-    $icon.addClass('online_icon');
-  } else {
-    // api连接测试
-    testCall();
-    // 每 5 分钟检测api连接状态
-    setInterval(testCall, 50000);
-  }
+  function selfTalk(str) {
+    const messageData = {
+      content: str,
+      time: formatAMPM(),
+      avatarSrc: '/img/_M.jpg'
+    };
 
-  if (1 == 2) {
-    // 测试用代码
-    testInit("如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); 后导致画面尺寸放大，可能有几种可能性需要考虑："
-      + "CSS动画效果导致的尺寸变化：该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，确保动画效果不会改变页面尺寸。"
-      + "jQuery动画函数引发的问题：在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。"
-      + "事件处理函数的影响：如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。确保事件处理函数没有意外的副作用。"
-      + "为了解决这个问题，您可以尝试以下方法：检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，尝试使用原生JavaScript代替jQuery来执行动画，"
-      + "看是否仍然出现同样的问题。如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，"
-      + "查看是否有任何意外的变化发生。通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); "
-      + "后导致画面尺寸放大，可能有几种可能性需要考虑：CSS动画效果导致的尺寸变化：该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，"
-      + "确保动画效果不会改变页面尺寸。jQuery动画函数引发的问题：在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。"
-      + "您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。事件处理函数的影响：如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。"
-      + "确保事件处理函数没有意外的副作用。为了解决这个问题，您可以尝试以下方法：检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，"
-      + "尝试使用原生JavaScript代替jQuery来执行动画，看是否仍然出现同样的问题。如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。"
-      + "在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何意外的变化发生。通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。"
-      + "如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); 后导致画面尺寸放大，可能有几种可能性需要考虑：CSS动画效果导致的尺寸变化："
-      + "该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，确保动画效果不会改变页面尺寸。jQuery动画函数引发的问题："
-      + "在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。事件处理函数的影响："
-      + "如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。确保事件处理函数没有意外的副作用。为了解决这个问题，您可以尝试以下方法："
-      + "检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，尝试使用原生JavaScript代替jQuery来执行动画，看是否仍然出现同样的问题。"
-      + "如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何意外的变化发生。"
-      + "通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。");
-  }
-});
+    const $msgContainer = $('<div>', { class: 'd-flex justify-content-end mb-4' });
+    const $imgContainer = $('<div>', { class: 'msg_cotainer_send' });
+    const $avatar = $('<img>', { src: messageData.avatarSrc, class: 'rounded-circle user_img_msg' });
+    const $msgContent = $('<div>', { class: 'img_cont_msg' });
+    const $message = $('<span>').text(messageData.content);
+    const $time = $('<span>', { class: 'msg_time_send' }).text(messageData.time);
 
+    // 提问信息显示
+    $imgContainer.append($message, $time);
+    $msgContent.append($avatar);
+    $msgContainer.append($imgContainer, $msgContent);
 
-// api连接测试并修改机器人在线状态
-function testCall() {
-  // api调用
-  $.ajax({
-    type: "GET",
-    url: "/Home/TryGPT",
-    data: null,
-    success: function (data) {
-      var $icon = $('#onlineStatus');
-      if (!/^2\d{2}$/.test(data.statusCode)) {
-        $icon.removeClass('online_icon');
-        $icon.addClass('offline');
-      } else {
-        $icon.removeClass('offline');
-        $icon.addClass('online_icon');
-      }
-    },
-    error: function () {
-      alert("出现错误,请重试");
-      $('.online_icon').addClass('offline');
-    }
-  });
-}
-
-// 立即停止打印文字
-let printIsStop = false;
-
-// 开始打印字符串
-function printString(content, $message, $chatBox) {
-  // 打印字符的时间间隔（毫秒）
-  var interval = 50;
-
-  var index = 0;
-  var timer = setInterval(function () {
-    // 添加一个字符到显示栏
-    var tmp = $message.text();
-    $message.text(tmp + content[index]);
-    index++;
+    // 将创建的元素追加到页面上合适的位置
+    $chatBox.append($msgContainer);
     // 滚动条移动至最后位置
     $chatBox.scrollTop($chatBox[0].scrollHeight);
 
-    // 如果所有字符都打印完毕，则清除定时器
-    if (index === content.length || printIsStop) {
-      $message.next().next().remove();
-      clearInterval(timer);
-    }
-  }, interval);
+  }
 
-}
+  function robotTalk(str) {
+    var $chatBox = $(".card-body");
 
-function formatAMPM() {
-  let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let ampm = hours >= 12 ? 'PM' : 'AM';
-  hours = hours % 12;
-  hours = hours ? hours : 12; // the hour '0' should be '12'
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  let strTime = hours + ':' + minutes + ' ' + ampm;
-  return strTime;
-}
+    const answerData = {
+      content: str,
+      time: formatAMPM(),
+      avatarSrc: '/img/_D.jpg'
+    };
+
+    const $msgContainer = $('<div>', { class: 'd-flex justify-content-start mb-4' });
+    const $msgContent = $('<div>', { class: 'img_cont_msg' });
+    const $avatar = $('<img>', { src: answerData.avatarSrc, class: 'rounded-circle user_img_msg' });
+    const $imgContainer = $('<div>', { class: 'msg_cotainer' });
+    const $message = $('<span>').text('');
+    const $time = $('<span>', { class: 'msg_time' }).text(answerData.time);
+    const $noPrint = $('<span>', { class: 'noPrint' }).text("STOP");
+
+    // 应答信息显示
+    $msgContent.append($avatar);
+    $imgContainer.append($message, $time, $noPrint);
+    $msgContainer.append($msgContent, $imgContainer);
+
+    // 将创建的元素追加到页面上合适的位置
+    $chatBox.append($msgContainer);
+    // 滚动条移动至最后位置
+    $chatBox.scrollTop($chatBox[0].scrollHeight);
+
+    // 文字打印
+    printString(answerData.content, $message, $chatBox);
+
+  }
+
+  // api连接测试并修改机器人在线状态
+  function chkOnline(msg) {
+    // api调用
+    $.ajax({
+      type: "GET",
+      url: "/Home/TryGPT",
+      data: null,
+      success: function (data) {
+        var $icon = $('#onlineStatus');
+        if (!/^2\d{2}$/.test(data.statusCode)) {
+          $icon.removeClass('online_icon');
+          $icon.addClass('offline');
+
+          if (msg != "" || msg != null) {
+            robotTalk(msg);
+          }
+        } else {
+          $icon.removeClass('offline');
+          $icon.addClass('online_icon');
+        }
+      },
+      error: function () {
+        alert("出现错误,请重试");
+      }
+    });
+  }
+
+  // 立即停止打印文字
+  let printIsStop = false;
+
+  // 开始打印字符串
+  function printString(content, $message, $chatBox) {
+    // 打印字符的时间间隔（毫秒）
+    var interval = 50;
+
+    var index = 0;
+    var timer = setInterval(function () {
+      // 添加一个字符到显示栏
+      var tmp = $message.text();
+      $message.text(tmp + content[index]);
+      index++;
+      // 滚动条移动至最后位置
+      $chatBox.scrollTop($chatBox[0].scrollHeight);
+
+      // 如果所有字符都打印完毕，则清除定时器
+      if (index === content.length || printIsStop) {
+        $message.next().next().remove();
+        clearInterval(timer);
+      }
+    }, interval);
+
+  }
+
+  function formatAMPM() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
 
 
-function testInit(str) {
-  var $chatBox = $(".card-body");
+  // ###########################################################
+  // 測試用
+  $block.on("click", function () {
 
-  const answerData = {
-    content: str,
-    time: formatAMPM(),
-    avatarSrc: '/img/_D.jpg'
-  };
+    const stringsArray =
+      [
+        "如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); 后导致画面尺寸放大，可能有几种可能性需要考虑："
+        + "CSS动画效果导致的尺寸变化：该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，确保动画效果不会改变页面尺寸。"
+        + "jQuery动画函数引发的问题：在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。"
+        + "事件处理函数的影响：如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。确保事件处理函数没有意外的副作用。"
+        + "为了解决这个问题，您可以尝试以下方法：检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，尝试使用原生JavaScript代替jQuery来执行动画，"
+        + "看是否仍然出现同样的问题。如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，"
+        + "查看是否有任何意外的变化发生。通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); "
+        + "后导致画面尺寸放大，可能有几种可能性需要考虑：CSS动画效果导致的尺寸变化：该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，"
+        + "确保动画效果不会改变页面尺寸。jQuery动画函数引发的问题：在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。"
+        + "您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。事件处理函数的影响：如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。"
+        + "确保事件处理函数没有意外的副作用。为了解决这个问题，您可以尝试以下方法：检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，"
+        + "尝试使用原生JavaScript代替jQuery来执行动画，看是否仍然出现同样的问题。如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。"
+        + "在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何意外的变化发生。通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。"
+        + "如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); 后导致画面尺寸放大，可能有几种可能性需要考虑：CSS动画效果导致的尺寸变化："
+        + "该动画可能会改变页面的布局或者尺寸，导致内容被放大。您可以检查相关的CSS样式，确保动画效果不会改变页面尺寸。jQuery动画函数引发的问题："
+        + "在某些情况下，jQuery的动画函数可能会导致页面尺寸变化。您可以尝试调整动画的参数或者使用其他方式来实现所需的效果。事件处理函数的影响："
+        + "如果该动画是作为事件处理函数的一部分执行的，可能是事件本身导致了页面尺寸变化。确保事件处理函数没有意外的副作用。为了解决这个问题，您可以尝试以下方法："
+        + "检查动画代码，确保它不会直接或间接地改变页面尺寸。如果可能的话，尝试使用原生JavaScript代替jQuery来执行动画，看是否仍然出现同样的问题。"
+        + "如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何意外的变化发生。"
+        + "通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。"
 
-  const $msgContainer = $('<div>', { class: 'd-flex justify-content-start mb-4' });
-  const $msgContent = $('<div>', { class: 'img_cont_msg' });
-  const $avatar = $('<img>', { src: answerData.avatarSrc, class: 'rounded-circle user_img_msg' });
-  const $imgContainer = $('<div>', { class: 'msg_cotainer' });
-  const $message = $('<span>').text('');
-  const $time = $('<span>', { class: 'msg_time' }).text(answerData.time);
-  const $noPrint = $('<span>', { class: 'noPrint' }).text("STOP");
+        , "如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，"
+        + "查看是否有任何意外的变化发生。通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。如果您发现在执行 $('html, body').animate({ scrollTop: 0 }, 80); "
+        + "后导致画面尺寸放大，可能有几种可能性需要考虑：CSS动画效果导致的尺寸变化：该动画可能会改变页面的布局或者尺寸，导致内容被放大"
 
-  // 应答信息显示
-  $msgContent.append($avatar);
-  $imgContainer.append($message, $time, $noPrint);
-  $msgContainer.append($msgContent, $imgContainer);
+        , "如果您确定问题是由于动画效果导致的，可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何意外的变化发生。"
+        + "通过仔细检查代码并排除可能的原因，您应该能够解决这个问题。"
 
-  // 将创建的元素追加到页面上合适的位置
-  $chatBox.append($msgContainer);
-  // 滚动条移动至最后位置
-  $chatBox.scrollTop($chatBox[0].scrollHeight);
+        , "确保事件处理函数没有意外的副作用。"
 
-  // 文字打印
-  printString(answerData.content, $message, $chatBox);
-}
+        , "确保动画效果不会改变页面尺寸。"
+
+        , "通过仔细检查代码并排除可能的原因。"
+
+        , "事件处理函数的影响。"
+
+        , "查看是否有任何意外的变化发生。"
+
+        , "可以尝试调整动画的参数或者更改实现方式。在动画执行之后，使用调试工具检查页面元素和样式，查看是否有任何"
+
+        , "abcdefhijklmnopqrstuvwxyz1234567890"
+
+        , "如果该动画是作为事件处理函数的一部分执行的，"
+
+      ];
+
+    const randomIndex = Math.floor(Math.random() * stringsArray.length);
+    const randomString = stringsArray[randomIndex];
+
+    $('.action_menu').toggle();
+    robotTalk(randomString);
+  });
+
+  // 測試連接
+  $checkOnline.on("click", function () {
+    $('.action_menu').toggle();
+
+    // api连接测试
+    chkOnline("我現在不在線，請稍等");
+    // 每 5 分钟检测api连接状态
+    //setInterval(chkOnline, 50000);
+
+  });
+
+});
+
+
+
+
+// ###########################################################
